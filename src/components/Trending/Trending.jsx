07262daa3642ff4205bddcd07a888a345fad33b2
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-07-07 00:57:01 +0300

    Implemented trending-now section | w/no animation yet

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 
 import "./trending.css";
+import { arrowDown } from "../../assets";
+import { Product } from "../";
+import { shoeProducts } from "../../data/products";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  return (
+    <section>
+      <div className="max-screen-width trending">
+        <div className="trending-header">
+          <div>
+            <h1>Trending Now</h1>
+            <p>
+              Discover the perfect blend of timeless style, quality, and
+              performance. Shop fotsie shoes, sneakers and gear.
+            </p>
+          </div>
+          <div className="sort-by">
+            <p>Sort by</p>
+            <p>
+              <img src={arrowDown} alt="" />
+            </p>
+          </div>
+        </div>
+
+        <div className="trending-products">
+          {shoeProducts
+            .filter((product) => product.trending)
+            .map((shoe, i) => (
+              <Product
+                key={i}
+                img={shoe.img}
+                name={shoe.name}
+                price={shoe.price}
+                category={shoe.category}
+              />
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Trending;
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as HeroCaroussel } from "./HeroCaroussel/HeroCaroussel";
 export { default as Trending } from "./Trending/Trending";
 export { default as Showcase } from "./Showcase/Showcase";
 export { default as Footer } from "./Footer/Footer";
+export { default as Product } from "./Product/Product";
